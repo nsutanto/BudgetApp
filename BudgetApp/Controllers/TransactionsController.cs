828595ec5359f25a59c0ec1f2c14ab56a84f6cb1
Nicholas Sutanto <gp912@cummins.com>
2017-05-08 22:15:51 -0400

    Adding transaction model

---
+++ BudgetApp/Controllers/TransactionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BudgetApp.Models;
+
+namespace BudgetApp.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Transactions
+        public IQueryable<Transaction> GetTransaction()
+        {
+            return db.Transaction;
+        }
+
+        // GET: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult GetTransaction(int id)
+        {
+            Transaction transaction = db.Transaction.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transaction);
+        }
+
+        // PUT: api/Transactions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTransaction(int id, Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != transaction.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(transaction).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Transactions
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult PostTransaction(Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Transaction.Add(transaction);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = transaction.Id }, transaction);
+        }
+
+        // DELETE: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult DeleteTransaction(int id)
+        {
+            Transaction transaction = db.Transaction.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            db.Transaction.Remove(transaction);
+            db.SaveChanges();
+
+            return Ok(transaction);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return db.Transaction.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- BudgetApp/Models/IdentityModels.cs
+++ BudgetApp/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
 
 namespace BudgetApp.Models
 {
@@ -20,6 +21,8 @@ namespace BudgetApp.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Transaction> Transaction { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
